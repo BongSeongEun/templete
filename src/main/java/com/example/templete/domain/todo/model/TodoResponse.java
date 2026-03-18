460ecd1607576ddac3794793f0388945f274fdae
BongSeongEun <bongfam2@naver.com>
2026-03-18 18:27:42 +0900

    dto class -> record 변경

--- src/main/java/com/example/templete/domain/todo/model/TodoResponse.java
+++ src/main/java/com/example/templete/domain/todo/model/TodoResponse.java
@@ -1,14 +1,11 @@
 package com.example.templete.domain.todo.model;
 
-import lombok.Getter;
-
 import java.sql.Timestamp;
 
-@Getter
-public class TodoResponse {
-    private Long todoId;
-    private String todoName;
-    private Timestamp todoStartTime;
-    private Timestamp todoEndTime;
-    private Boolean todoStatus;
+public record TodoResponse (
+    Long todoId,
+    String todoName,
+    Timestamp todoStartTime,
+    Timestamp todoEndTime,
+    Boolean todoStatus){
 }
