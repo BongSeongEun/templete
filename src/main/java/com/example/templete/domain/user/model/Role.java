306f5b4a876d00e71df3f8645d78d998104ab4f2
BongSeongEun <bongfam2@naver.com>
2026-03-19 12:27:12 +0900

    권한 이름 수정

--- src/main/java/com/example/templete/domain/user/model/Role.java
+++ src/main/java/com/example/templete/domain/user/model/Role.java
@@ -1,6 +1,6 @@
 package com.example.templete.domain.user.model;
 
 public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN
+    USER,
+    ADMIN
 }
--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -6,7 +6,7 @@ create table user
     user_name      varchar(20)                     not null,
     login_id       varchar(50)                     not null,
     login_password varchar(255)                     not null,
-    role           varchar(20) default 'ROLE_USER' not null,
+    role           varchar(20) default 'USER' not null,
     constraint table_name_pk_2
         unique (user_uuid),
     constraint table_name_pk_3
